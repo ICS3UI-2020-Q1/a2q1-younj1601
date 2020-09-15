0b996790ac7a893df40d2c94275976ba9d8609c3
younj1601 <younj1601@wrdsb.ca>
2020-09-15 14:09:23 +0000

    Finished the first question for conditions

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program determines whether a number is even or odd
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //prompt for interger
+    System.out.println("Please enter an integer:");
+
+    //get interger from user
+    int num = input.nextInt();
+
+    //calculate remainder
+    int remainder = num % 2;
+
+    if(remainder > 0){
+      //tell them that the number is odd
+      System.out.println(num + " is an odd number");
+    } else
+    // tell them that the number is even
+    System.out.println(num + " is an even number");
     
   }
 }
